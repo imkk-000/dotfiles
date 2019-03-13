a255817ce89e762bebac5eea22604ed98c204203
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-13 22:52:53 +0700

    [Added]: copy all config to user directory in install-my-fresh-linux.sh

--- install-my-fresh-linux.sh
+++ install-my-fresh-linux.sh
@@ -1,25 +1,23 @@
 # version
+NODE_VERSION=10
 GO_VERSION=1.12
 
 # enable sudo
 sudo whoami >> /dev/null
-# end enable sudo
 
 # update and upgrade
 sudo apt-get update && sudo apt-get upgrade -y
-# end update and upgrade
 
 # install tools
 sudo apt-get install -y $(check-language-support)
 sudo apt-get install -y git-core
+sudo apt-get install -y gcc g++ python make cmake autoconf python-pip
 sudo apt-get install -y wget curl xclip traceroute whois ufw tree \
                         net-tools dirmngr apt-transport-https ca-certificates \
                         xinput \
                         zsh tmux vim gnome-tweak-tool dconf-editor \
                         gufw gparted htop p7zip-full software-properties-common \
                         unzip zip nmap transmission vlc gimp
-sudo apt-get install -y gcc g++ python make cmake autoconf python-pip
-# end install tools
 
 # install vscode
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
@@ -27,7 +25,6 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" 
       | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt-get update
 sudo apt-get install -y code
-# end install vscode
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -36,7 +33,6 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -c
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo usermod -aG docker $USER
-# end install docker
 
 # install google chrome
 curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -44,7 +40,6 @@ echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" \
       | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
-# end install google chrome
 
 # install spotify-client
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
@@ -52,7 +47,6 @@ echo "deb [arch=amd64] http://repository.spotify.com stable non-free" \
       | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install -y spotify-client
-# end install spotify-client
 
 # install virtualbox
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -
@@ -61,17 +55,14 @@ echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_r
       | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
 sudo apt-get install -y virtualbox
-# end install virtualbox
 
 # install node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
 sudo apt-get install -y nodejs
-# end install node
 
 # install golang
 wget -qO- https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz | sudo tar -C /opt/ -xzf -
 sudo mv /opt/go /opt/golang
-# end install golang
 
 # create directory
 mkdir -p $HOME/.icons \
@@ -81,4 +72,10 @@ mkdir -p $HOME/.icons \
          $HOME/.go \
          $HOME/.ssh \
          $HOME/.vim
-# end create directory
+mkdir -p $HOME/Documents/linux \
+         $HOME/Documents/golang \
+         $HOME/Documents/node \
+         $HOME/Pictures/Profiles
+
+# copy all config to user directory
+cp .myconfig/.* $HOME/
